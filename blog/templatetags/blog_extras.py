37b01e6780db16867addd0730d6685ccb81fadce
Dharshini Dharshini <bdharshini31@gmail.com>
2024-12-19 06:10:13 +0000

    Finish custom template tags

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,10 +1,13 @@
 from django.contrib.auth import get_user_model
 from django import template
+from blog.models import post
 from django.utils.html import escape
 from django.utils.html import format_html
 from django.utils.safestring import mark_safe
 register=template.Library()
 user_model=get_user_model()#get user model instance
+#custom filter to return first and lastname else username as a link so that they can be emailed to when clicked
+#used formatting for escaping and providing safe data to include in html
 @register.filter
 def author_details(author,current_user):
   if not isinstance(author,user_model):
@@ -23,3 +26,18 @@ def author_details(author,current_user):
     preifx=""
     suffix=""
   return format_html('{}{}{}',prefix,name,suffix)
+#custom tags for row and endrow
+@register.simple_tag
+def row(extra_classes=""):
+  return format_html('<div class="row {}">',extra_classes)
+  
+@register.simple_tag
+def endrow():
+  return format_html('</div>')
+
+@register.inclusion_tag("blog/post-list.html")
+def recent_posts(posts):
+  p=post.objects.exclude(pk=posts.pk)[:5]
+  return {"title":"Recent posts","post":p}#always returns back a dictonary item
+
+
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from blog.models import post
 from django.utils import timezone
 def index(request):
-  posts=post.objects.filter(published_at__lte=timezone.now())
-  return render(request,"blog/index.html",{"post":posts})
-# Create your views here.
+  p=post.objects.filter(published_at__lte=timezone.now())
+  return render(request,"blog/index.html",{"posts":p})
+
+def post_detail(request,slug):
+  p=get_object_or_404(post,slug=slug)
+  return render(request,"blog/post-details.html",{"post":p})
